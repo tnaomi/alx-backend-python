a0fe956abebe67fd52b32d2215981165e14244ec
tnaomi <naomikapengule@gmail.com>
2025-10-19 08:42:56 +0200

    Created dummy views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def getChats(request):
+    return render(request, 'chats/chat_list.html')
+
+def getChat(request, chat_id):
+    return render(request, 'chats/chat.html', {'chat_id': chat_id})
+
