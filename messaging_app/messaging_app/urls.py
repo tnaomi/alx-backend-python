7371405042bb02a3c78b7f28dfbc88758495b4ea
tnaomi <naomikapengule@gmail.com>
2025-10-19 19:30:47 +0200

    Changed base url for chats urls to api

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chats/', include('chats.urls')),
+    path('api/', include('chats.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
