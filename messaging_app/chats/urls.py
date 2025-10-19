f0af3447c5433c868e10527e19612c0e5dfb39b3
tnaomi <naomikapengule@gmail.com>
2025-10-19 15:20:04 +0200

    Added more explicit endpoints for CR

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('messages/', views.MessageViewSet.as_view({'get': 'list', 'post': 'create'}), name='messages'),
+    path('messages/', views.MessageViewSet.as_view({'get': 'list'}), name='messages'),
+    path('messages/create/', views.MessageViewSet.as_view({'post': 'create'}), name='create_message'),
     path('conversations/', views.ConversationViewSet.as_view({'get': 'list'}), name='conversations'),
+    path('conversations/add', views.ConversationViewSet.as_view({ 'post': 'create'}), name='create-conversation')
 ]
